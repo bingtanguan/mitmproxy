1bf8d79c7abe5e0a1a6d8a08da97696754668d6b
Run Ou <run.ou@secneo.com>
2015-12-02 18:55:51 -0800

    d

--- mysqlHelp.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-
-def connect():
-    try:
-        conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='mitmproxy',port=3306)
-        return conn
-    except MySQLdb.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
